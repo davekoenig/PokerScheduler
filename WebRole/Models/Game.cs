05a56d4e0685b3b2d8d4f5fbf7193327feb4e378
Dave Koenig <davek@microsoft.com>
2016-04-28 21:43:17 -0700

    adding game and player controller

---
+++ WebRole/Models/Game.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace WebRole.Models
+{
+    internal class Game : TableEntity
+    {
+        internal DateTime StartTime { get; set; }
+
+        internal DateTime EndTime { get; set; }
+
+        internal string RoomName { get; set; }
+
+        internal string GameId { get; private set; }
+
+        private HashSet<Player> _players = new HashSet<Player>();
+
+        internal ReadOnlyCollection<Player> Players
+        {
+            get
+            {
+                return _players.ToList().AsReadOnly();
+            }
+        }
+
+        internal Game()
+        {
+            GameId = Guid.NewGuid().ToString();
+
+            RowKey = GameId;
+
+            PartitionKey = StartTime.ToLongDateString();
+        }
+
+        internal void AddPlayer(Player player)
+        {
+            _players.Add(player);
+        }
+
+        internal void RemovePlayer(Player player)
+        {
+            _players.Remove(player);
+        }
+    }
+}
---
+++ WebRole/Models/Player.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRole.Models
+{
+    internal class Player : TableEntity, IEquatable<Player>
+    {
+        internal string FirstName { get; set; }
+        internal string LastName { get; set; }
+
+        internal string Alias { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Player other = obj as Player;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return (other.FirstName == FirstName) && (other.LastName == LastName);
+        }
+
+        public bool Equals(Player other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Equals((object)other);
+        }
+
+        public static bool operator==(Player left, Player right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator!=(Player left, Player right)
+        {
+            return !(left == right);
+        }
+
+        public override int GetHashCode()
+        {
+            return FirstName.GetHashCode() ^ LastName.GetHashCode() * 37;
+        }
+    }
+}
