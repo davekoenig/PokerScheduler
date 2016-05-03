76889c279ef969ae3b1e78aae77cfd24349c3f9c
Dave Koenig <davek@microsoft.com>
2016-05-02 18:41:48 -0700

    added DataManager Singleton

---
+++ WebRole/Data/DataManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WebRole.Data
+{
+    public class DataManager
+    {
+        private static Lazy<DataManager> _instance = new Lazy<DataManager>(() => new DataManager());
+
+        public static DataManager Instance => _instance.Value;
+
+        private DataManager()
+        {
+
+        }
+
+        public static IDataProvider Provider { get; }
+    }
+}
